b3583303b750d88555f0c60682299350f04d40b9
Justin Seiter <justinseiter@gmail.com>
2022-03-13 14:54:33 -0500

    Adds paging

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -1,46 +1,68 @@
 import {useEffect, useState} from "react";
 
 function Exchanges() {
+    const apiBaseUrl = "https://api.coingecko.com/api/v3/exchanges?per_page=10"
     const [exchanges, setExchanges] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [totalPages, setTotalPages] = useState(0)
+
     useEffect(() => {
-       fetch('https://api.coingecko.com/api/v3/exchanges?per_page=10')
-           .then(res => res.json())
+        setLoading(true);
+       fetch(`${apiBaseUrl}&page=${page}`)
+           .then(res => {
+               const headers = [...res.headers];
+               setTotalPages(headers[4][1] / 10);
+               return res.json()
+           })
            .then(exchanges => {
-               setExchanges(exchanges)
+               setExchanges(exchanges);
+               setLoading(false);
            });
-    }, []);
+    }, [page]);
+
+    if(loading) return <>Loading...</>;
 
     return(
         <div>
             <h1>Exchanges</h1>
             { exchanges.length ?
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Trust Rank</th>
-                        <th>Name</th>
-                        <th>Country</th>
-                        <th>Website</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    { exchanges.map(exchange => {
-                        return (
-                            <tr>
-                                <td>{exchange.trust_score_rank}</td>
-                                <td>
-                                    <img src={exchange.image} width="30px"/>
-                                    {exchange.name}
-                                </td>
-                                <td>{exchange.country}</td>
-                                <td>{exchange.url}</td>
-                            </tr>
-                        )
-                    })}
-                    </tbody>
-                </table>
+                <>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Trust Rank</th>
+                            <th>Name</th>
+                            <th>Country</th>
+                            <th>Website</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        { exchanges.map(exchange => {
+                            return (
+                                <tr key={exchange.trust_score_rank}>
+                                    <td>{exchange.trust_score_rank}</td>
+                                    <td>
+                                        <img alt="" src={exchange.image} width="30px"/>
+                                        {exchange.name}
+                                    </td>
+                                    <td>{exchange.country}</td>
+                                    <td>{exchange.url}</td>
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </table>
+                    <div>
+                        <button onClick={() => setPage(1)}>First</button>
+                        <button onClick={() => setPage(page - 1)}>Previous</button>
+                        <span>{page} of {totalPages}</span>
+                        <button onClick={() => setPage(page + 1)}>Next</button>
+                        <button onClick={() => setPage(totalPages)}>Last</button>
+                    </div>
+                </>
                 :
-                <>Loading</>
+                null
             }
 
         </div>
